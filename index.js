5c2b473086a67e3fd4f41da00cdc7fadb6c44aa0
pcomo24 <pcomo24@gmail.com>
2017-06-10 00:03:45 -0500

    ugly but done

--- index.js
+++ index.js
@@ -6,12 +6,39 @@ var promise = require('bluebird');
 var pgp = require('pg-promise')({
   promiseLib: promise
 });
+var session = require('express-session');
+var pbkdf2 = require('pbkdf2');
+var crypto = require('crypto');
+
 //databse connection
 var db = pgp({database: 'ToDo'})
 app.use(body_parser.urlencoded({extended: false}));
-
 //hbs view engine
 app.set('view engine', 'hbs');
+//session middleware initialized
+app.use(session({
+  secret: process.env.SECRET_KEY || 'dev',
+  resave: true,
+  saveUninitialized: false,
+  cookie: {maxAge: 60000}
+}));
+//session object
+app.use(function (req, res, next) {
+  if(req.session.user) {
+    next();
+  } else if (req.path == '/login'){
+    next();
+  } else {
+    res.redirect('/login');
+  }
+})
+//password encryption
+var salt = crypto.randomBytes(20).toString('hex');
+var password = 'some-password';
+var key = pbkdf2.pbkdf2Sync(
+  password, salt, 36000, 256, 'sha256'
+);
+var hash = key.toString('hex');
 
 //URLS for views
 app.get('/', function (req, res) {
@@ -54,7 +81,20 @@ app.get('/todos/done/:id', function(req, res) {
       res.redirect('/todos')
     })
   });
-
+//user login
+app.get('/login', function (req, res) {
+  response.render('login.hbs');
+});
+app.post('/login', function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  if (username == 'aaron' && password == 'narf') {
+    req.session.user = username;
+    res.redirect('/');
+  } else {
+    res.render('login.hbs');
+  }
+});
 //server start and connect
 app.listen(8500, function () {
   console.log('8500 gon head witcha order')
